1ffab2507fbc55526c8a2849c3b51695905dae94
YingJung03 <ingridlin0303@gmail.com>
2024-06-29 22:18:02 +0800

    225.Implement_Stack_using_Queues.cpp

---
+++ Leetcode/225.Implement_Stack_using_Queues.cpp
@@ -0,0 +1,31 @@
+class MyStack {
+public:
+queue<int>q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+       q.push(x);
+       for(int i=0;i<q.size()-1;i++)
+       {
+        q.push(q.front());
+        q.pop();
+       }
+    }
+    
+    int pop() {
+        int val=q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+        
+    }
+    
+    bool empty() {
+        return q.empty();        
+    }
+};
