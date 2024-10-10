c3c9a58ad1f31b7ca4f07fb0e163040676da3947
YingJung03 <ingridlin0303@gmail.com>
2024-10-10 15:16:44 +0800

    26.Remove_Duplicates_from_Sorted_Array.cpp

---
+++ Leetcode/26.Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j=1;
+        if(nums.empty()){
+            return 0;
+        }
+        
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){
+                nums[j]=nums[i];
+                j++;
+                
+            }
+        }
+        return j;
+    }   
+};
