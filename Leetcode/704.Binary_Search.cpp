2533bf3a9143db07cd00a01226218144c06689f4
YingJung03 <ingridlin0303@gmail.com>
2024-11-23 10:55:50 +0800

    704.Binary_Search.cpp

---
+++ Leetcode/704.Binary_Search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0;
+        int right=nums.size()-1;
+        
+        while (left <= right){
+            int mid=(left+right)/2; // 用 int 的性質做無條件捨去
+            if(nums[mid]>target){
+                right = mid - 1;
+            }else if(nums[mid]<target){
+                left=mid+1;   
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
